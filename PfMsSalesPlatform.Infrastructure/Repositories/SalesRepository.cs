18ccf4c1bacf6fdcb25b7962226ae1fdc9dc5635
MateoArenas <mateo3197@gmail.com>
2024-03-05 19:19:20 -0500

    Add Repository and UnitOfWork Pattern

---
+++ PfMsSalesPlatform.Infrastructure/Repositories/SalesRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PfMsSalesPlatform.Infrastructure.Context;
+
+namespace PfMsSalesPlatform.Infrastructure.Repositories
+{
+    public class SalesRepository<T> : ISalesRepository<T> where T : class
+    {
+        private readonly SalesDBContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public SalesRepository(SalesDBContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task<List<T>> GetAll() => await _dbSet.ToListAsync();
+
+        public async Task<T?> GetAsync(int Id) => await _dbSet.FindAsync(Id);
+
+        public async Task<T?> InsertAsync(T entity)
+        {
+            var entityResult = await _dbSet.AddAsync(entity);
+
+            return entityResult.Entity;
+        }
+
+        public async Task<T?> UpdateAsync(int Id, T entity)
+        {
+            var findEntity = await _dbSet.FindAsync(Id);
+
+            if (findEntity == null)
+                return null;
+
+            _dbContext.Entry(findEntity).CurrentValues.SetValues(entity);
+
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(int Id)
+        {
+            var findEntity = await _dbSet.FindAsync(Id);
+
+            if (findEntity == null)
+                return false;
+
+            _dbSet.Remove(findEntity);
+
+            return true;
+        }
+    }
+}
---
+++ PfMsSalesPlatform.Infrastructure/Repositories/UnitWork/UnitOfWork.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PfMsSalesPlatform.Infrastructure.Context;
+
+namespace PfMsSalesPlatform.Infrastructure.Repositories.UnitWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly SalesDBContext _DBContext;
+        private bool _disposed;
+
+        public UnitOfWork(SalesDBContext dBContext)
+        {
+            _DBContext = dBContext;
+        }
+
+        public void Commit()
+        {
+            _DBContext.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+            foreach (var entry in _DBContext.ChangeTracker.Entries())
+            {              
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Reload();
+                        break;
+                    case EntityState.Modified:
+                        entry.State = EntityState.Detached;
+                        break;
+                }
+            }
+        }
+
+        public ISalesRepository<T> SalesRepository<T>() where T : class
+        {
+            return new SalesRepository<T>(_DBContext);
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _DBContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
